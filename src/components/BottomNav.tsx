0bdc30aa14423267f5a6c58b4abdfa8fedbba587
oramasdigital12 <oramasdiguital12@gmail.com>
2025-04-30 17:48:19 -0400

    fix: corregir error de tipado en BottomNav usando location.pathname

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -1,4 +1,4 @@
-import { NavLink } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { FiHome, FiUsers, FiDollarSign, FiSettings, FiCalendar } from 'react-icons/fi';
 import { HomeIcon, UserGroupIcon, CurrencyDollarIcon, Cog6ToothIcon } from '@heroicons/react/24/outline';
 
@@ -11,19 +11,19 @@ const navItems = [
 ];
 
 export default function BottomNav() {
+  const location = useLocation();
+
   return (
     <nav className="fixed bottom-0 left-0 right-0 bg-white border-t shadow-lg flex justify-around items-center h-16 z-50 md:static md:h-auto md:shadow-none md:border-none md:bg-transparent">
       {navItems.map(item => (
-        <NavLink
+        <Link
           key={item.to}
           to={item.to}
-          className={({ isActive }) =>
-            `flex flex-col items-center justify-center text-xs font-medium transition-colors ${isActive ? 'text-blue-600' : 'text-gray-500'}`
-          }
+          className={`flex flex-col items-center justify-center text-xs font-medium transition-colors ${location.pathname === item.to ? 'text-blue-600' : 'text-gray-500'}`}
         >
           <span className="text-xl mb-1">{item.icon}</span>
           {item.label}
-        </NavLink>
+        </Link>
       ))}
     </nav>
   );
